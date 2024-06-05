278f7e23171dbcb09e393791c91ee9455263ecc3
hurst831028 <hurst831028@163.com>
2024-06-05 18:49:41 +0800

    Update the integration of react-unity-webgl

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import { Unity, useUnityContext } from "react-unity-webgl";
+
+// function App() {
+//   const { unityProvider } = useUnityContext({
+//     loaderUrl: "build/StageBuild.loader.js",
+//     dataUrl: "build/StageBuild.data",
+//     frameworkUrl: "build/StageBuild.framework.js",
+//     codeUrl: "build/StageBuild.wasm",
+//   });
+
+//   return <Unity unityProvider={unityProvider} />;
+// }
+
+import React, { Fragment , useCallback, useEffect, useState} from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+    loaderUrl: "build/StageBuild.loader.js",
+    dataUrl: "build/StageBuild.data.unityweb",
+    frameworkUrl: "build/StageBuild.framework.js.unityweb",
+    codeUrl: "build/StageBuild.wasm.unityweb",
+  });
+
+  const [devicePixelRatio, setDevicePixelRatio] = useState<undefined | number>(undefined);
+  const [canvasWidth, setCanvasWidth] = useState<number | undefined>();
+  const [canvasHeight, setCanvasHeight] = useState<number | undefined>();
+
+  // const handleSendAddress = useCallback(() => {
+  //   sendMessage('UnityNotify', 'SetWalletAddress', address);
+  // }, [address, isConnected]);
+
+  useEffect(() => {
+    setDevicePixelRatio(window.devicePixelRatio);
+  }, []);
+
+  useEffect(
+    function () {
+      // A function which will update the device pixel ratio of the Unity
+      // Application to match the device pixel ratio of the browser.
+      const updateDevicePixelRatio = function () {
+        setDevicePixelRatio(window.devicePixelRatio);
+      };
+      // A media matcher which watches for changes in the device pixel ratio.
+      const mediaMatcher = window.matchMedia(`screen and (resolution: ${devicePixelRatio}dppx)`);
+      // Adding an event listener to the media matcher which will update the
+      // device pixel ratio of the Unity Application when the device pixel
+      // ratio changes.
+      mediaMatcher.addEventListener('change', updateDevicePixelRatio);
+      return function () {
+        // Removing the event listener when the component unmounts.
+        mediaMatcher.removeEventListener('change', updateDevicePixelRatio);
+      };
+    },
+    [devicePixelRatio]
+  );
+
+  useEffect(() => {
+    const updateWidthAndHeight = () => {
+      setCanvasWidth(window.innerWidth);
+      setCanvasHeight(window.innerHeight - 64);
+    };
+    updateWidthAndHeight();
+
+    window.addEventListener('resize', updateWidthAndHeight);
+    return () => {
+      window.removeEventListener('resize', updateWidthAndHeight);
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Fragment>
+      {!isLoaded && (
+        <p>loading...{Math.round(loadingProgression * 100)}%</p>
+      )}
+      <Unity
+        unityProvider={unityProvider}
+        devicePixelRatio={devicePixelRatio}
+        style={{
+          width: canvasWidth,
+          height: canvasHeight,
+        }}
+      />
+    </Fragment>
+  );
 }
+
 
 export default App
